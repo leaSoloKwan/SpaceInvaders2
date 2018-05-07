1331bad31dd952d25088c76f34ae6fd0332bfbb2
SOLO KWAN <leask99@hotmail.fr>
2018-05-07 17:25:42 +0200

    Positionner vaisseau dimension

--- src/main/java/fr/unilim/iut/SpaceInvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/SpaceInvaders/Vaisseau.java
@@ -3,14 +3,42 @@ package fr.unilim.iut.SpaceInvaders;
 public class Vaisseau {
 	int x;
 	int y;
+	int longueur;
+	int hauteur;
 
-	public Vaisseau(int x, int y) {
+	public Vaisseau(int longueur, int hauteur) {
+		this(longueur, hauteur, 0, 0);
+	}
+
+	public Vaisseau(int longueur, int hauteur, int x, int y) {
+		this.longueur=longueur;
+		this.hauteur=hauteur;
 		this.x = x;
 		this.y = y;
 	}
 
 	public boolean occupeLaPosition(int x, int y) {
-		return (this.x==x) && (this.y==y);
+	     return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
+    }
+
+	private boolean estOrdonneeCouverte(int y) {
+		return (ordonneeLaPlusBasse()<=y) && (y<=ordonneeLaPlusHaute());
+	}
+
+	private int ordonneeLaPlusBasse() {
+		return ordonneeLaPlusHaute()-this.hauteur+1;
+	}
+
+	private int ordonneeLaPlusHaute() {
+		return this.y;
+	}
+
+	private boolean estAbscisseCouverte(int x) {
+		return (abscisseLaPlusAGauche()<=x) && (x<=abscisseLaPlusADroite());
+	}
+
+	public int abscisseLaPlusADroite() {
+		return this.x+this.longueur-1;
 	}
 
 	public void seDeplacerVersLaDroite() {
@@ -18,7 +46,7 @@ public class Vaisseau {
 		
 	}
 
-	public int abscisse() {
+	public int abscisseLaPlusAGauche() {
 		
 		return this.x;
 	}
@@ -27,4 +55,9 @@ public class Vaisseau {
 		this.x=this.x-1;
 		
 	}
+
+	public void positionner(int x, int y) {
+	    this.x = x;
+	    this.y = y;
+    }
 }
